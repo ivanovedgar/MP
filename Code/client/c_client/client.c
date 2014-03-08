974ab1ef2970c04030350dcc15c6e0cca3541071
Edgar Ivanov <ivanovedgar@gmail.com>
2014-03-08 23:24:27 +0000

    Relays can be switched on and off now.

--- Code/client/c_client/client.c
+++ Code/client/c_client/client.c
@@ -1,27 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include ".\box_api.h"
+#include "box_api.h"
 #include "client.h"
+
+void do_thing();
+void playWithRelays();
+void menu();
+int checkValidity(int,int,int);
 
 int main()
 {
    int s;
    char hostname[BUFFSIZE];
    int port_number;
-
+/*
    printf("WHICH HOST COMPUTER ?:");
    scanf("%s",hostname);
    printf("WHICH port number ?:");
    scanf("%d", &port_number);
 
    box_connect(hostname,port_number);
-
-   do_things(s);
-
+*/
+	box_connect("192.168.1.40",4675);
+    while(1){
+	menu();
+	}
    box_disconnect();
-
+   
    return EXIT_SUCCESS;
+}
+
+void menu(){
+	int choice, relay;
+	unsigned int status;
+	float incl[2] =  {0.0,0.0};
+	printf("This is a menu for demo\n");
+	printf("1 Set all relays\n");
+	printf("2 Reset all relays\n");
+	printf("3 Set relay N\n");
+	printf("4 Reset relay N\n");
+	printf("5 Play with relays\n");
+	printf("6 Get relay N status\n");
+	printf("7 Get status of all relays\n");
+	printf("8 Get inclinometer values\n");
+	scanf("%d", &choice);
+
+	if(checkValidity(1,8, choice) == 1){	
+		
+		switch(choice){
+		case 1:
+				setRelays(255);
+				break;
+		case 2:
+				setRelays(0);
+				break;
+		case 3:
+				printf("Eneter relay number to set\n");
+				scanf("%d", &relay);
+				setRelayN(relay);
+				break;
+		case 4:
+				printf("Eneter relay number to reset\n");
+				scanf("%d", &relay);
+				
+				resetRelayN(relay);
+				break;
+		case 5:
+				playWithRelays();
+				break;
+		case 6:
+				printf("Enter relay number from 0 to 7\n");
+				scanf("%d", &relay);
+				status = getRelayN(relay);
+				printf("The status is %d\n", status);
+				break;
+		case 7:
+				status = getRelays();
+				printf("The status of all relays is %d\n", status);
+				break;
+		case 8:
+				getInclinometer(incl);
+                printf("side-side= %f fore-aft = %f\n", incl[0], incl[1]);
+		}
+	}else{
+	printf("Wrong entry, try again values from 0 to 7\n");
+	choice = 0;
+	}
+}
+
+int checkValidity(int bottomValue, int topVallue, int suppliedValue){
+	int response;
+
+	if((suppliedValue >= bottomValue) && (suppliedValue <= topVallue)){
+	response = 1;
+	}else{
+	response = 0;
+	}
+	return response;
+}
+
+void playWithRelays(){
+	int relayCount;
+	int i =0;
+		while(i < 3){
+			for(relayCount = 0; relayCount <= 7; relayCount++){
+			resetRelayN(relayCount);
+		}
+			for(relayCount = 0; relayCount <=7; relayCount++){
+			setRelayN(relayCount);
+			}
+			i++;
+		}
+	setRelays(0);
+}
+
+void do_thing(){
+	int choice;
+	while(1){
+		printf("Hello choose option\n");
+		printf("1 Reset Relay N\n");
+		printf("2 Set Relay N\n");
+		scanf("%d", &choice);
+		if(choice == 1){
+			int relayToReset;
+			printf("What relay you want to reset?");
+			scanf("%d", &relayToReset);
+			resetRelayN(relayToReset);
+		}
+		else if(choice == 2){
+			int relayToSet;
+			printf("What relay you want to set?");
+			scanf("%d", &relayToSet);
+			setRelayN(relayToSet);
+		}
+	}
 }
 
 void do_things() {
